ae509267ddcb0c6d63fb7bb16915798692e30400
cbrooks718 <charliebrooks@Charlies-MacBook-Air.local>
2023-01-23 15:13:25 -0600

    Uploading Deliverable 2

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,24 +1,24 @@
 -- Deliverable 1
 
 -- Creating retirement_titles table by joining employees and titles based on emp_no
--- SELECT e.emp_no, e.first_name, e.last_name, ti.title, ti.from_date, ti.to_date
--- INTO retirement_titles
--- FROM titles as ti
--- INNER JOIN employees as e
--- ON (ti.emp_no = e.emp_no)
--- WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
--- ORDER BY emp_no;
+SELECT e.emp_no, e.first_name, e.last_name, ti.title, ti.from_date, ti.to_date
+INTO retirement_titles
+FROM titles as ti
+INNER JOIN employees as e
+ON (ti.emp_no = e.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
 
 -- Use Distinct with Orderby to remove duplicate rows & create unique titles csv
--- SELECT DISTINCT ON (emp_no) emp_no,
--- first_name,
--- last_name,
--- title
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
 
--- INTO unique_titles
--- FROM retirement_titles
--- WHERE to_date = '9999-01-01'
--- ORDER BY emp_no, to_date DESC;
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
 
 -- Create retiring titles csv
 
@@ -27,3 +27,15 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY title
 ORDER BY COUNT DESC;
+
+-- Deliverable 2
+-- Creating Employees eligible for Mentorship table
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
